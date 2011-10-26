9ccf3d460f6b01fe10d31a5847185d7b91d94027
Jarrett Baugh <jarrett.baugh@gmail.com>
2011-10-26 13:11:27 -0700

    Used better way of reading responses thanks to boost example

--- downloader.cpp
+++ downloader.cpp
@@ -65,19 +65,20 @@ void Downloader::_download(FileToDownload file)
 		boost::system::error_code ignored_error;
 		boost::asio::write(socket, boost::asio::buffer(request.str()), boost::asio::transfer_all(), ignored_error);
 
-		// Read response
-		char buf[1024];
-		size_t len = socket.read_some(boost::asio::buffer(buf), error);
-		if (error == boost::asio::error::eof) {
-				return; 
-		}
-		else if (error) {
-				throw boost::system::system_error(error);
-		}
+		// Read headers
+		boost::asio::streambuf response_buf;
+		std::stringstream response_stream;
+    boost::asio::read_until(socket, response_buf, "\r\n\r\n");
+
+    // Read content
+    std::string response = "";
+    while(boost::asio::read(socket, response_buf, boost::asio::transfer_at_least(1), error)) {
+      	response_stream << &response_buf;
+    }
 
 		// Parse out the headers
-		std::string response = buf;
-		response = response.substr(response.find("\r\n\r\n") + 4);
+		response = response_stream.str();
+		response = response_stream.str().substr(response.find("\r\n\r\n") + 4);
 
 		// Make the temporary directories and then save the file
 		FileManager::mkdirs("tmp" + local_dir);
